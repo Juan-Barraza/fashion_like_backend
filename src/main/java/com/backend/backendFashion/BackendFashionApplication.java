ba0bc57be13cd499fb0d3316130aa42b0ae8edc7
Deiver Vasquez <deiver.vasquezm@gmail.com>
2025-02-23 17:56:23 -0500

    init project

---
+++ src/main/java/com/backend/backendFashion/BackendFashionApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backendFashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/backendFashion/BackendFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.backendFashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
