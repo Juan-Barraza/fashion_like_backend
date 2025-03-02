2cba3c7cdd7a2e0315c957aa16893cf9d6357001
Juan Barraza <juanbarraza2004@outlook.com>
2025-03-02 14:47:04 -0500

    feat: add dockerfile and docker compose

--- src/main/java/com/backend/backendFashion/BackendFashionApplication.java
+++ src/main/java/com/backend/backendFashion/BackendFashionApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendFashionApplication {
 
 	public static void main(String[] args) {
+        // Si deseas, puedes imprimir alguna variable para verificar la carga:
+		String appName = System.getenv("SPRING_APPLICATION_NAME");
+        String dbName  = System.getenv("POSTGRES_DB");
+
+        System.out.println("Nombre de la aplicación: " + appName);
+        System.out.println("Postgres DB: " + dbName);
 		SpringApplication.run(BackendFashionApplication.class, args);
 	}
 
